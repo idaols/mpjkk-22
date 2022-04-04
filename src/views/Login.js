7abe84f9e06dd44dc20186240766a4e97ec09e78
idaols <ida.olsson@metropolia.fi>
2022-04-04 09:18:51 +0300

    redi

--- src/views/Login.js
+++ src/views/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
   const [toggle, setToggle] = useState(true);
   return (
     <>
-      {toggle ? <LoginForm /> : <RegisterForm />}
+      {toggle ? <LoginForm /> : <RegisterForm setToggle={setToggle} />}
       {toggle ? `Don't have account?` : `Already have an account?`}
       <Button
         fullWidth
